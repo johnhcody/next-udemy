e5a92f6d819ae48c233f64581e55832e59134056
johnhcody <john.haner.cody@gmail.com>
2021-01-04 13:50:29 +0700

    add about

---
+++ pages/about.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+    
+}
+
+const About = (props: Props) => {
+    return (
+        <div>
+            hello
+        </div>
+    )
+}
+
+export default About
